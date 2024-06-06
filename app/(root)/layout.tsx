cf2243774f40283173f62709cf4c70cc36d12b61
Hopewell Motsamai <hpwlmtsm@gmail.com>
2024-06-06 16:32:56 +0200

    acq

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,9 +8,9 @@ export default function RootLayout({
   return (
     <div>
         <main>
-            <p className="text-black-1">LEFT SIDEBAR</p>
+            <LeftSidebar />
             {children}
-            <p className="text-black-1">RIGHT SIDEBAR</p>
+            <p className="text-white-1">RIGHT SIDEBAR</p>
         </main>
     </div>
   );
--- app/(root)/podcast/[podcastid]/page.tsx
+++ app/(root)/podcast/[podcastid]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PodcastDetails = ({ params }: { params: { podcastid: string }}) => {
   return (
-    <p className='text-black-1'>PodcastDetails For {params.podcastid}</p>
+    <p className='text-white-1'>PodcastDetails For {params.podcastid}</p>
   )
 }
 
